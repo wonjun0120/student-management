89e41a93addf998f5ba90c651b6cfea8e5e54e0d
wonjun0120 <wonjundero@gmail.com>
2021-02-17 11:59:44 +0900

    add environ

--- studentmanagement/app/__init__.py
+++ studentmanagement/app/__init__.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 app.config.from_object('config')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = ''
+app.secret_key = os.environ['SECRET']
 db = SQLAlchemy(app)
 
 from app import views, models
--- studentmanagement/config.py
+++ studentmanagement/config.py
@@ -1,3 +1,4 @@
-# SQLALCHEMY_DATABASE_URI = 'mysql://b750afc5e6e6e8:bcb7a751@us-cdbr-east-03.cleardb.com/heroku_5b8c6bfa7f20b08?charset=utf8'
+import os
 
-SQLALCHEMY_DATABASE_URI = 'mysql://root:1234@localhost/tt?charset=utf8'
+SQLALCHEMY_DATABASE_URI = os.environ['CLEARDB_DATABASE_URL']
+
